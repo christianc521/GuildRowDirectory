0468d4f95912c57d5fba83aefee4d208d84e7448
Christian Cruz <seseko98@gmail.com>
2023-03-23 13:18:43 -0500

    preproduction edits

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../styles/globals.css';
+import { store } from "../src/app/store";
+import { Provider } from 'react-redux'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+        <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
